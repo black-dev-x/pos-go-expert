2e40c760812f5b615ae788b5909ee020ecfec03a
Thiago <thiago.dev.x@gmail.com>
2025-02-20 16:01:01 -0300

    Update context timeout in main.go

--- 29_context/3_client/main.go
+++ 29_context/3_client/main.go
@@ -12,6 +12,8 @@ func main() {
 	// server is currently 5 seconds, we could test a timeout from the server,
 	// a timeout from the client or a success by changing the timer
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) success
+	//ctx, cancel := context.WithTimeout(context.Background(),  3*time.Second) error
 	defer cancel()
 	req, error := http.NewRequestWithContext(ctx, "GET", "http://localhost:8081", nil)
 	if error != nil {
