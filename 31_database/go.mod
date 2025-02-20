a2ad0046e71affc9075fbc0870b18ff2b1dd93aa
Thiago <thiago.dev.x@gmail.com>
2025-02-20 17:40:50 -0300

    Update dependencies and add database functionality

--- 31_database/go.mod
+++ 31_database/go.mod
@@ -2,4 +2,9 @@ module github.com/black-dev-x/pos-go-expert/31_database
 
 go 1.21.3
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.9.0
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- 31_database/main.go
+++ 31_database/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -17,5 +22,23 @@ func NewProduct(id, name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/database")
+	if err != nil {
+		panic(err)
+	}
+	insertProduct(db, NewProduct("1", "Product 1", 9.99))
+	defer db.Close()
+}
+
+func insertProduct(db *sql.DB, product *Product) {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, error := stmt.Exec(product.ID, product.Name, product.Price)
+	if error != nil {
+		panic(error)
+	}
 
 }
