a74c208d5a4287a36a83cb0d111e650a443f1b2a
Thiago <thiago.dev.x@gmail.com>
2025-02-21 14:51:19 -0300

    commit

---
+++ 33_gorm_many_to_many/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	Name       string
+	Price      float64
+	Categories []Category `gorm:"many2many:product_categories;"`
+	gorm.Model
+}
+
+// belongs to
+type Category struct {
+	ID       uint `gorm:"primarykey"`
+	Name     string
+	Products []Product `gorm:"many2many:product_categories;"`
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/database?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	// kitchen := Category{Name: "Kitchen"}
+	// db.Create(&kitchen)
+	// electronics := Category{Name: "Electronics"}
+	// db.Create(&electronics)
+
+	// product := Product{Name: "Potato Killer", Price: 1.0, Categories: []Category{kitchen, electronics}}
+	// db.Create(&product)
+
+	products := []Product{}
+	db.Preload("Categories").Find(&products)
+	for _, product := range products {
+		println(product.Name)
+		for _, category := range product.Categories {
+			println(" - ", category.Name)
+		}
+	}
+}
