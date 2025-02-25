03c02162e03a9211ff0030c10f077c8e8f82d735
Thiago <thiago.dev.x@gmail.com>
2025-02-25 16:34:41 -0300

    benchmark

--- 38_go_test_batch/tax.go
+++ 38_go_test_batch/tax.go
@@ -1,7 +1,17 @@
 package tax
 
+import "time"
+
 func CalculateTax(amount float64) float64 {
-	if amount >= 1000 {
+	if amount >= 1000.0 {
+		return 10.0
+	}
+	return 5.0
+}
+
+func CalculateTax2(amount float64) float64 {
+	time.Sleep(time.Millisecond)
+	if amount >= 1000.0 {
 		return 10.0
 	}
 	return 5.0
