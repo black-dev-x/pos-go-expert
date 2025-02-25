03c2105ef874f8a42f27f614318aa2ebd2a7aec2
Thiago <thiago.dev.x@gmail.com>
2025-02-25 15:34:59 -0300

    Refactor variable names and update comments

---
+++ 34_go_mod/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/black-dev-x/pos-go-expert/34_go_mod/math"
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+	result := m.Add()
+	println(result)
+	println("Hello, World!")
+}
---
+++ 34_go_mod/go.mod
@@ -0,0 +1,3 @@
+module github.com/black-dev-x/pos-go-expert/34_go_mod
+
+go 1.24.0
