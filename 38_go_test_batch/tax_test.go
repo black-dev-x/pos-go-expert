fd4068288980636a465ce6c5f3223f19153819db
Thiago <thiago.dev.x@gmail.com>
2025-02-25 16:22:44 -0300

    tests

---
+++ 38_go_test_batch/tax_test.go
@@ -0,0 +1,31 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0 // 5.0 will pass, 6.0 will fail
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %.2f, but got %.2f", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+	for _, v := range table {
+		result := CalculateTax(v.amount)
+		if result != v.expected {
+			t.Errorf("Expected %.2f, but got %.2f", v.expected, result)
+		}
+	}
+}
