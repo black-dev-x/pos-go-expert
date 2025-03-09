20d59a0bbd91d43160e205658204e992ce778e66
Thiago <thiago.dev.x@gmail.com>
2025-03-08 21:47:50 -0300

    testify

---
+++ 40_go_testify/tax.go
@@ -0,0 +1,24 @@
+package tax
+
+import "time"
+
+func CalculateTax(amount float64) float64 {
+	if amount <= 0 {
+		return 0
+	}
+	if amount < 1000 {
+		return 5
+	}
+	if amount < 20000 {
+		return 10
+	}
+	return 20
+}
+
+func CalculateTax2(amount float64) float64 {
+	time.Sleep(time.Millisecond)
+	if amount >= 1000.0 {
+		return 10.0
+	}
+	return 5.0
+}
