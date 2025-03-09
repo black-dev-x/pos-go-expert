1f4a58356e67f5a35bd0a2c689274a04d66a807f
Thiago <thiago.dev.x@gmail.com>
2025-03-08 22:10:33 -0300

    update

--- 41_go_testify_mock/tax_test.go
+++ 41_go_testify_mock/tax_test.go
@@ -11,10 +11,10 @@ func TestCalculateTax(t *testing.T) {
 	repository := &TaxRepositoryMock{}
 	repository.On("SaveTax", 10.0).Return(nil)
 	repository.On("SaveTax", 0.0).Return(errors.New("amount should be greater than 0"))
-
 	error := CalculateTaxAndSave(1000.0, repository)
 	assert.Nil(t, error)
 
 	error = CalculateTaxAndSave(0.0, repository)
 	assert.Error(t, error, "amount should be greater than 0")
+	repository.AssertExpectations(t)
 }
