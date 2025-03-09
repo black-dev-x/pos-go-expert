fad498f2bae6dce27a6b47b9a96a38541aca151f
Thiago <thiago.dev.x@gmail.com>
2025-03-08 22:12:11 -0300

    update

--- 41_go_testify_mock/tax_test.go
+++ 41_go_testify_mock/tax_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestCalculateTax(t *testing.T) {
@@ -16,5 +17,18 @@ func TestCalculateTax(t *testing.T) {
 
 	error = CalculateTaxAndSave(0.0, repository)
 	assert.Error(t, error, "amount should be greater than 0")
+	repository.AssertExpectations(t)
+}
+
+func TestCalculateTaxWithMockAnything(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", mock.Anything).Return(nil)
+	error := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, error)
+
+	error = CalculateTaxAndSave(0.0, repository)
+	assert.Nil(t, error)
+	error = CalculateTaxAndSave(2000.0, repository)
+	assert.Nil(t, error)
 	repository.AssertExpectations(t)
 }
